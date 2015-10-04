421576439949cb19f7c5ad9063693ab3de7548d7
Jenjeur <alex.prokopowicz@gmail.com>
2015-10-04 22:40:33 +0200

    Commit fantôme
    Pomf

---
+++ src/main/java/fr/prokopowicz/alex/listeners/PlayerJoinTextListener.java
@@ -0,0 +1,15 @@
+package fr.prokopowicz.alex.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Created by Alexandre on 03/06/2015.
+ */
+public class PlayerJoinTextListener implements Listener{
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        event.getPlayer().sendMessage("Pomf");
+    }
+}
