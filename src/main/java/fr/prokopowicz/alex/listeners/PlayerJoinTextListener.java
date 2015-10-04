c392092f62d95d5428db5a484c8075f2f27d8acf
Jenjeur <alex.prokopowicz@gmail.com>
2015-10-04 23:00:35 +0200

    Ajout de texte (sisi)
    - Ajout d'un texte Rouge et SOULIGNE qui dit "Splotch"
    - Ajout d'un texte qui dit merci et quelques autres conneries inutiles

--- src/main/java/fr/prokopowicz/alex/listeners/PlayerJoinTextListener.java
+++ src/main/java/fr/prokopowicz/alex/listeners/PlayerJoinTextListener.java
@@ -1,5 +1,6 @@
 package fr.prokopowicz.alex.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,5 +12,7 @@ public class PlayerJoinTextListener implements Listener{
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         event.getPlayer().sendMessage("Pomf");
+        event.getPlayer().sendMessage("" + ChatColor.RED + ChatColor.UNDERLINE + "Splotch");
+        event.getPlayer().sendMessage("Thanks for using ReadOnlyWarning V.-1 Please report any troubleshooting at 06.42.69.42.69");
     }
 }
