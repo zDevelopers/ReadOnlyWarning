c54fdca22b9425791599a90c585af40ef76b082f
Alexandre <alex.prokopowicz@gmail.com>
2016-08-28 12:30:47 +0200

    Added periodic message for Read Only players
    Oui on have switched du français à l'english, so quoi?

--- src/main/java/fr/prokopowicz/alex/ReadOnlyWarning.java
+++ src/main/java/fr/prokopowicz/alex/ReadOnlyWarning.java
@@ -2,14 +2,14 @@ package fr.prokopowicz.alex;
 
 import fr.prokopowicz.alex.commands.ReadOnlyCommand;
 import fr.prokopowicz.alex.listeners.PlayerInteractionListener;
-import fr.prokopowicz.alex.listeners.PlayerJoinTextListener;
+import fr.prokopowicz.alex.listeners.PlayerWarningListener;
 import fr.prokopowicz.alex.managers.ReadOnlyPlayersManager;
-import org.bukkit.plugin.java.JavaPlugin;
+import fr.zcraft.zlib.core.ZPlugin;
 
 /**
  * Created by Alexandre on 03/06/2015.
  */
-public class ReadOnlyWarning extends JavaPlugin {
+public class ReadOnlyWarning extends ZPlugin {
 
 	private static ReadOnlyWarning instance;
 
@@ -18,7 +18,7 @@ public class ReadOnlyWarning extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		instance = this;
-		getServer().getPluginManager().registerEvents(new PlayerJoinTextListener(), this);
+		getServer().getPluginManager().registerEvents(new PlayerWarningListener(), this);
 		getServer().getPluginManager().registerEvents(new PlayerInteractionListener(), this);
 		getServer().getPluginCommand("ro").setExecutor(new ReadOnlyCommand());
 
---
+++ src/main/java/fr/prokopowicz/alex/listeners/PlayerWarningListener.java
@@ -0,0 +1,40 @@
+package fr.prokopowicz.alex.listeners;
+
+import fr.prokopowicz.alex.ReadOnlyWarning;
+import fr.prokopowicz.alex.rawtypes.ReadOnlyPlayer;
+import fr.zcraft.zlib.tools.runners.RunTask;
+import fr.zcraft.zlib.tools.text.Titles;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.UUID;
+
+/**
+ * Created by Alexandre on 03/06/2015.
+ */
+public class PlayerWarningListener implements Listener{
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        final UUID cot = event.getPlayer() .getUniqueId();
+        RunTask.timer(new Runnable() {
+            @Override
+            public void run() {
+                ReadOnlyPlayer ericsaget = ReadOnlyWarning.getInstance() .getReadOnlyPlayersManager() .getReadOnlyPlayer(cot);
+                Player pomme = ReadOnlyWarning.getInstance() .getServer() .getPlayer(cot);
+                if (ericsaget != null && pomme != null && pomme .isOnline())
+                {
+                    pomme.sendMessage("");
+                    pomme.sendMessage(ChatColor.BOLD + "You've been placed in read only, asshole");
+                    pomme.sendMessage("Reason:" + ericsaget.getReason());
+                    pomme.sendMessage("Please explain yourself on the forum at https://forum.zcraft.fr/viewtopic.php?id=1980");
+                    pomme.sendMessage("");
+
+                    Titles.displayTitle(pomme, 10, 120, 10, ChatColor.RED + "READ ONLY (waa)", "");
+                }
+            }
+        }, 2L, 10*60*20);
+    }
+}
